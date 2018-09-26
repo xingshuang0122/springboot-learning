c6fec6e42991389a6de9e2fa5b7d4c1ff14a82f5
Shuang <xingshuang_cool@163.com>
2018-09-26 22:53:47 +0800

    feat(project): 添加数据库的几个接口

--- src/main/java/com/huibo/demo/mapper/UserMapper.java
+++ src/main/java/com/huibo/demo/mapper/UserMapper.java
@@ -12,10 +12,9 @@ package com.huibo.demo.mapper;
 
 
 import com.huibo.demo.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * 〈一句话功能简述〉<br>
@@ -36,6 +35,15 @@ public interface UserMapper {
     User findByName(@Param("username") String username);
 
     /**
+     * 根据对象查询用户
+     *
+     * @param user 对象
+     * @return 用户对象
+     */
+    @Select("SELECT * FROM testuser WHERE USERNAME = #{username}")
+    User findByName1(User user);
+
+    /**
      * 插入用户
      *
      * @param username 用户名
@@ -44,4 +52,19 @@ public interface UserMapper {
      */
     @Insert("INSERT INTO testuser(USERNAME,PASSWORD) VALUES(#{username}, #{password})")
     Integer insertUser(@Param("username") String username, @Param("password") String password);
+
+
+    /**
+     * 查询所有用户
+     *
+     * @return 用户列表
+     */
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "username", column = "username"),
+            @Result(property = "password", column = "password")
+    })
+    @Select("SELECT * FROM testuser")
+    List<User> findAllUsers();
+
 }
--- src/test/java/com/huibo/demo/DemoApplicationTests.java
+++ src/test/java/com/huibo/demo/DemoApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -24,8 +26,14 @@ public class DemoApplicationTests {
     @Test
     public void findByName() {
         User user = this.userMapper.findByName("xing");
-        System.out.println(user);
+//        System.out.println(user);
         Assert.assertEquals("77", user.getPassword());
+
+        User user1 = new User();
+        user1.setUsername("哈哈哈");
+        User user2 = this.userMapper.findByName1(user1);
+//        System.out.println(user2);
+        Assert.assertEquals("爱空间管控闹人", user2.getPassword());
     }
 
     @Test
@@ -34,5 +42,13 @@ public class DemoApplicationTests {
         Integer result = this.userMapper.insertUser("测试", "123456");
         System.out.println(result);
         Assert.assertEquals(1, result.intValue());
+    }
+
+    @Test
+    public void findAllUsers() {
+        List<User> users = this.userMapper.findAllUsers();
+        for (User item : users) {
+            System.out.println(item);
+        }
     }
 }
