b8274f633b32705817a01555ec2b84cf3c15b71a
Shuang <xingshuang_cool@163.com>
2018-09-28 21:41:57 +0800

    feat(project): SpringBoot的端点测试

---
+++ src/main/java/com/huibo/demo/health/TestHealthIndicator.java
@@ -0,0 +1,43 @@
+/*
+  Copyright (C), 2017-2018, 江苏汇博机器人技术股份有限公司
+  FileName: TestHealthIndicator
+  Author:   xingshuang
+  Date:     2018/9/28 21:33
+  Description: 自定义健康节点
+  History:
+  <author>          <time>          <version>          <desc>
+  作者姓名           修改时间           版本号              描述
+ */
+package com.huibo.demo.health;
+
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 自定义健康节点
+ *
+ * @author xingshuang
+ * @date 2018/9/28 21:33
+ */
+@Component("test")
+public class TestHealthIndicator implements HealthIndicator {
+    private static final String VERSION = "1.0.0";
+
+    @Override
+    public Health health() {
+        int code = check();
+        if (code != 0) {
+            Health.down().withDetail("code", code).withDetail("version", VERSION).build();
+        }
+        return Health.up().withDetail("code", code)
+                .withDetail("version", VERSION).up().build();
+
+    }
+
+    private Integer check(){
+        return 0;
+    }
+}
