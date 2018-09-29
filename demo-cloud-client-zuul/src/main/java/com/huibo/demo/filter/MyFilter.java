a68389a444d2bafa9b964a684c6a68c633268881
Shuang <xingshuang_cool@163.com>
2018-09-29 17:22:26 +0800

    feat(demo-cloud-client-zuul): Spring Cloud 路由网关（zuul）(Finchley版本)，还有服务过滤器的实现

---
+++ demo-cloud-client-zuul/src/main/java/com/huibo/demo/filter/MyFilter.java
@@ -0,0 +1,67 @@
+/*
+  Copyright (C), 2017-2018, 江苏汇博机器人技术股份有限公司
+  FileName: MyFilter
+  Author:   ShuangPC
+  Date:     2018/9/29 17:15
+  Description: 
+  History:
+  <author>         <time>          <version>          <desc>
+  作者姓名         修改时间           版本号             描述
+ */
+
+package com.huibo.demo.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <一句话功能简述> <br>
+ *
+ * @author 邢双
+ * @date 2018/9/29 17:15
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
