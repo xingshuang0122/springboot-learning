2c4b197dd950cb5d9f3116fdf67afaa97a9b6a88
Shuang <xingshuang_cool@163.com>
2019-01-24 22:40:35 +0800

    feat(project): 1# 添加mybatis自动生成的工程；2# 添加mybatis-generator生成器的gui程序

---
+++ demo-mybatis-generate/src/main/java/com/huibo/demo/DemoMybatisGenerateApplication.java
@@ -0,0 +1,14 @@
+package com.huibo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMybatisGenerateApplication.class, args);
+    }
+
+}
+
